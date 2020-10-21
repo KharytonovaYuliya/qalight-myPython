fb5889046942bc6ac0244bab996d9e3360d9e0d1
Yuliya <kharytonova.yuliya@gmail.com>
2020-10-22 01:33:23 +0300

    Первый и Последний!

---
+++ Первый и Последний!.py
@@ -0,0 +1,18 @@
+def firstandlast(a,b,c,d,e,f,g,h,i,j,k):
+    list1 = [a,b,c,d,e,f,g,h,i,j,k]
+    print("First item in the list was: " + list1.__getitem__(0))
+    print("Last item in the list was: " + list1.__getitem__(10))
+
+
+a1 = input("Enter A: ")
+b1 = input("Enter B: ")
+c1 = input("Enter C: ")
+d1 = input("Enter D: ")
+e1 = input("Enter E: ")
+f1 = input("Enter F: ")
+g1 = input("Enter G: ")
+h1 = input("Enter H: ")
+i1 = input("Enter I: ")
+j1 = input("Enter J: ")
+k1 = input("Enter K: ")
+firstandlast(a1,b1,c1,d1,e1,f1,g1,h1,i1,j1,k1)
